024bb937023b9487a5a386b3103701e49200fcfc
Dmytro Kharchenko <dimonkharchenko89@gmail.com>
2017-03-21 18:23:55 +0200

    some update

---
+++ js/script.js
@@ -0,0 +1,44 @@
+$(function(){
+	
+	var myBlock = $("#my-block__script").html();
+	var blockQuestion = [
+		{
+			question: 'Вопрос №1',
+			answer: [
+				'Ответ 1',
+				'Ответ 2',
+				'Ответ 3'
+			],
+			correct: 1,
+		},
+		{
+			question: 'Вопрос №2',
+			answer: [
+				'Ответ 4',
+				'Ответ 5',
+				'Ответ 6'
+			],
+			correct: 1,
+		},
+		{
+			question: 'Вопрос №3',
+			answer: [
+				'Ответ 7',
+				'Ответ 8',
+				'Ответ 9'
+			],
+			correct: 1,
+		}
+	
+	];
+
+
+	var quest = tmpl(myBlock, { 
+		data: blockQuestion
+	 });
+	 
+	$('.script-block').append(quest);
+
+
+    // blockQuestion.question[0].answer[blockQuestion.question[0].correct]
+});
